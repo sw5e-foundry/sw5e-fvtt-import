30ed3bd5e037736b3d14f0a4bb4e2db5f04e9c16
Ikaguia <Ikaguia@users.noreply.github.com>
2023-06-03 20:43:26 -0300

    Fix

--- sw5e/EnhancedItem.py
+++ sw5e/EnhancedItem.py
@@ -186,9 +186,9 @@ class EnhancedItem(sw5e.Entity.Item):
 		# Use the modification subtype icons
 		if self.is_modification:
 			subtype = self.raw_subtype
-			if self.raw_type == 'CyberneticAugmentation': subtype = f'CyberneticMod'
-			elif self.raw_type == 'DroidCustomization': subtype = f'DroidMod'
-			return f'systems/sw5e/packs/Icons/Modifications/{subtype.replace(" ", "").capitalize()}Mod.svg'
+			if self.raw_type == 'CyberneticAugmentation': subtype = f'Cybernetic'
+			elif self.raw_type == 'DroidCustomization': subtype = f'Droid'
+			return f'systems/sw5e/packs/Icons/Modifications/{subtype.replace(" ", "").capitalize()}Mod.webp'
 
 		# Otherwise use the default item bag icon
 		return 'icons/svg/item-bag.svg'
