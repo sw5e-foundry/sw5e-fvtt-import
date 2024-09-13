067e57137107770e81c0e57704c26999a9cb88b5
Ikaguia <Ikaguia@users.noreply.github.com>
2024-09-13 19:44:36 -0300

    Maneuvers

--- sw5e/Maneuvers.py
+++ sw5e/Maneuvers.py
@@ -6,7 +6,7 @@ class Maneuvers(sw5e.Feature.BaseFeature):
 		return super().getAttrs() + [ "metadata", "type", "eTag" ]
 
 	def getType(self):
-		return "maneuver"
+		return "sw5e.maneuver"
 
 	def getFeatType(self):
 		return None, None
@@ -16,12 +16,17 @@ class Maneuvers(sw5e.Feature.BaseFeature):
 		return f'modules/sw5e/icons/packs/Maneuvers/{name}.webp'
 
 	def getAction(self):
-		return utils.text.getAction(self.raw_text, self.name, rolled_formula='@scale.superiority.die')
+		return utils.text.getAction(self.raw_text, self.name, rolled_formula='1d@superiority.die')
 
 	def getData(self, importer):
 		data = super().getData(importer)[0]
 
 		data["system"]["type"] = { "value": self.raw_type.lower() }
+		data["consume"] = {
+			"amount": 1,
+			"target": 'superiority.dice.value',
+			"type": 'attribute',
+		}
 
 		return [data]
 
